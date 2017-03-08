fdb828238aaf7daad69f60dc2855bcdd5a2f0b7e
USER <USER@ASUS>
2017-03-08 20:51:13 +0800

    wds 01

---
+++ Myjava/src/tw/iii/matt/matt030802.java
@@ -0,0 +1,18 @@
+package tw.iii.matt;
+
+public class matt030802 {
+
+	public static void main(String[] args) {
+		int input = 5;
+		int remain = input %2;
+		if(remain==1){
+        System.out.printf("%d為基數",input);
+      
+		}
+		else {
+			System.out.printf("%d為偶數",input);
+			
+		}
+	}
+
+}
