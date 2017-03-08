b7df66d53159344a6a73fb0e034e32b57200d4ed
USER <USER@123.240.214.80>
2017-03-08 11:03:05 +0800

    03080 1100

---
+++ Myjava/src/tw/iii/matt/matt030801.java
@@ -0,0 +1,27 @@
+package tw.iii.matt;
+
+import java.util.Scanner;
+
+public class matt030801 {
+
+	public static void main(String[] args) {
+	    int n1, n2 ,n3 ;
+		Scanner Scanner = new Scanner(System.in);
+	    
+	    System.out.println("請輸入數字");
+	    n1 = Scanner.nextInt();
+	    n2 = Scanner.nextInt();
+	    n3 = Scanner.nextInt();
+	    		
+	    System.out.println("以書"+n1);
+	    System.out.println("以書"+n2);
+	    System.out.println("以書"+n3);
+	    
+	    		
+		
+		
+		 
+
+	}
+
+}
